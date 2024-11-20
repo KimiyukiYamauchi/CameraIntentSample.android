54d493fd474c4a80af54488ffd33c9858a0405a2
Kimiyuki Yamauchi <kimiyuki.yamauchi.0313@gmail.com>
2024-11-20 14:06:08 +0900

    first commit

---
+++ app/src/main/java/com/example/cameraintentsample/MainActivity.java
@@ -0,0 +1,99 @@
+
+package com.example.cameraintentsample;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    // Cameraアクティビティを起動するためのランチャーオブジェクト
+    ActivityResultLauncher<Intent> _cameraLauncher =
+            registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallbackFromCamera()
+            );
+    private Uri _imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // 親クラスの同名メソッドの呼び出し
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    // 画像部分がタップされた時の処理メソッド
+    public void onCameraImageClick (View view) {
+        // 日時データを「yyyyMMddHHmmss」の形式に整形するフォーマッタを生成
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+        // 現在の日時を取得
+        Date now = new Date(System.currentTimeMillis());
+        // 取得した日時データを「yyyyMMddHHmmss」形式に整形した文字列を生成
+        String nowStr = dateFormat.format(now);
+        // ストレージに格納する画像のファイル名を生成。ファイルの一意を確保するために
+        // タイムスタンプの値を利用
+        String fileName = "CameraIntentSamplePhoto_" + nowStr + ".jpg";
+
+        // ContentValuesオブジェクトを生成
+        ContentValues values = new ContentValues();
+        // 画像ファイル名を設定
+        values.put(MediaStore.Images.Media.TITLE, fileName);
+        // 画像ファイルの種類を設定
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        // ContentResolverオブジェクトを生成
+        ContentResolver resolver = getContentResolver();
+        // ContentResolverを使ってURIオブジェクトを生成
+        _imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        // Intentオブジェクトを生成
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Extra情報として、_imageUriを設定
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri);
+        // アクティビティを起動
+//        startActivityForResult(intent, 200);
+        _cameraLauncher.launch(intent);
+    }
+
+    // Cameraアクティビティから戻ってきたときの処理が記述されたコールバッククラス
+    private class ActivityResultCallbackFromCamera implements
+            ActivityResultCallback<ActivityResult> {
+
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            // カメラアプリで撮影成功の場合
+            if (result.getResultCode() == RESULT_OK) {
+                // 画像を表示するImageViewを取得
+                ImageView ivCamera = findViewById(R.id.ivCamera);
+                // フィールドの画像URIをImageViewに設定
+                ivCamera.setImageURI(_imageUri);
+
+                // メディアスキャナーに通知
+                Intent scanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                scanIntent.setData(_imageUri);
+                sendBroadcast(scanIntent);
+            }
+        }
+    }
+}
